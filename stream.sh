a716309cb7f9aebbf0e2be133a6e443871587d46
Colin Winslow <colinwinslow@gmail.com>
2022-10-30 22:36:57 -0700

    Update stream.sh
    adds configurable port for icecast

--- stream.sh
+++ stream.sh
@@ -3,4 +3,4 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-ffmpeg -re ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL} ${FFMPEG_OUTPUT_OPTS} -c:a libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 icecast://source:${ICECAST_PASSWORD}@${ICECAST_URL}:8000/${ICECAST_MOUNTPOINT}
+ffmpeg -re ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL} ${FFMPEG_OUTPUT_OPTS} -c:a libmp3lame -b:a 128k -content_type audio/mpeg -f mp3 icecast://source:${ICECAST_PASSWORD}@${ICECAST_URL}:${ICECAST_PORT}/${ICECAST_MOUNTPOINT}
